5a8512eaf40ef3cdf237f74e4a593e635be77eff
Ruoshui <rmao10@stuy.edu>
2020-01-03 00:34:13 -0500

    maybe working

--- semctl/semctl.c
+++ semctl/semctl.c
@@ -1,17 +1,17 @@
-union semun {
-               int              val;    /* Value for SETVAL */
-               struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-               unsigned short  *array;  /* Array for GETALL, SETALL */
-               struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                                           (Linux-specific) */
-           };
-
-#define SHMKEY 123456789
-
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int main (int argc, char * [] argv)
+#include <sys/types.h>
+#include <sys/ipc.h>
+
+#include <sys/sem.h>
+
+#include <sys/shm.h>
+
+#include "semctl.h"
+
+int main(int argc, char *argv[])
 {
     // there should be only one input and it should have len == 2, and should be in format "-[]"
     if (argc != 2 || strlen(argv[1]) != 2 || argv[1][0] != '-')
@@ -20,38 +20,128 @@ int main (int argc, char * [] argv)
         return EXIT_FAILURE;
     }
 
+    int semd = -1;
+    int shmd = -1;
+
     switch (argv[1][1])
     {
-        case 'c':
+    case 'c':
+        ;
+        // make semaphore
+        semd = semget(SEMKEY, 1, IPC_CREAT | 0644);
+        if (semd == -1)
+        {
+            perror("semget");
+            return EXIT_FAILURE;
+        }
+
+        puts("Semaphore created");
+        // init sem val
+        union semun arg;
+        arg.val = 0;
+        if (semctl(semd, 0, SETVAL) != 0)
+        {
+            perror("semctl");
+            return EXIT_FAILURE;
+        }
 
         // make shared mem
+        if (shmget(SHMKEY, SHMSIZE, IPC_CREAT | 0644) == -1)
+        {
+            perror("shmget");
+            return EXIT_FAILURE;
+        }
 
-        // make semaphore
+        puts("Shared memory created");
 
         // make file
+        FILE *file = fopen(FILENAME, "w");
+        if (!file)
+        {
+            perror("fopen");
+            return EXIT_FAILURE;
+        }
 
-            break;
+        puts("File created");
+        fclose(file);
 
-        case 'r':
+        break;
+
+    case 'r':
+        ;
+        semd = semget(SEMKEY, 1, 0);
+        if (semd == -1)
+        {
+            perror("semget");
+            return EXIT_FAILURE;
+        }
+        struct sembuf op;
+        op.sem_flg = 0;
+        op.sem_num = 0;
+
+        // wait for sem
+        op.sem_op = 0;
+        if (semop(semd, &op, 1) == -1)
+        {
+            perror("semop");
+            return EXIT_FAILURE;
+        }
+
+        // now sem released, increase it
+        op.sem_op = 1;
+        if (semop(semd, &op, 1) == -1)
+        {
+            perror("semop");
+            return EXIT_FAILURE;
+        }
         // display the whole file
+        display_file();
+
+        // op.sem_op = -1;
+        // if (semop(semd, &op, 1) == -1)
+        // {
+        //     perror("semop");
+        //     return EXIT_FAILURE;
+        // }
 
         // remove semaphore, shared mem, and file
+        semctl(semd, 0, IPC_RMID);
+        
+        puts("semaphore removed");
 
+        shmd = shmget(SHMKEY, SHMSIZE, 0);
+        if (shmd == -1)
+        {
+            perror("shmget");
+            return EXIT_FAILURE;
+        }
+        if (shmctl(shmd, IPC_RMID, 0) != 0)
+        {
+            perror("shmctl");
+            return EXIT_FAILURE;
+        }
+        
+        if (remove(FILENAME) != 0)
+        {
+            perror("remove");
+            return EXIT_FAILURE;
+        }
 
+        break;
 
-
-
-            break;
-
-        case 'v':
-
+    case 'v':
         // display the whole file
 
-            break;
-        default:
-            printf("Unrecognized option: %s\n", argv[1]);
-            return EXIT_FAILURE;
+        display_file();
+        fclose(file);
+
+        break;
+    default:
+        printf("Unrecognized option: %s\n", argv[1]);
+        return EXIT_FAILURE;
     }
+
+    return EXIT_SUCCESS;
 }
 
 void print_help()
@@ -64,4 +154,21 @@ void print_help()
     puts("\t-r\tdisplay the story and remove the story file");
     puts("\t-v\tdisplay the story");
     puts("");
+}
+
+void display_file()
+{
+    FILE *f = fopen(FILENAME, "r");
+    if (!f)
+    {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    puts("The story so far:");
+    char c;
+    while ((c = fgetc(f)) != EOF)
+    {
+        fputc(c, stdout);
+    }
+    fclose(f);
 }
---
+++ semctl/semctl.h
@@ -0,0 +1,16 @@
+#define SEMKEY 121454
+#define SHMKEY 123456
+#define SHMSIZE sizeof(int)
+
+#include <stdio.h>
+#define FILENAME "story.txt"
+union semun {
+               int              val;    /* Value for SETVAL */
+               struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+               unsigned short  *array;  /* Array for GETALL, SETALL */
+               struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                                           (Linux-specific) */
+           };
+
+void display_file();
+void print_help();
